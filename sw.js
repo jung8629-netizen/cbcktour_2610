5bdf11dfb784052222616ed9eb4c3ffe6f943bd2
jung8629-netizen <jung8629@gmail.com>
2026-08-23 23:56:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reform-tour-v3';
+const CACHE = 'reform-tour-v5';
 const ASSETS = ['./','./index.html','./manifest.webmanifest',
                 './icon-192.png','./icon-512.png','./icon-180.png','./icon-maskable.png'];
 
